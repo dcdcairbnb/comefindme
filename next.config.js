ad64a3ff9838a211c16bf261f264438b89edc81f
Dan C <dcdcairbnb@gmail.com>
2026-08-21 22:22:36 -0500

    Configure webpack alias for postgres
    Add webpack alias for @vercel/postgres to pg-shim.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@vercel/postgres'] = path.resolve(
+      __dirname,
+      'lib/pg-shim.js'
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
